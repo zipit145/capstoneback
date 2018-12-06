7fa65b8b551e1ae2b6713a3edced21eb3dab2555
zipit145 <gregbforney@gmail.com>
2018-12-06 14:08:14 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app  = express()
+const bodyparser = require('body-parser')
+const cors = require('cors')
+const port = process.env.PORT || 3003
+
+app.use(cors())
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: true}))
+//app.use('/students', students)
+
+app.listen(port, () => {
+    console.log("running")
+})
